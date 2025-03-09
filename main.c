5a51b969eba14c0ed449936657d7dbe3282bc00d
Pepseman727 <pepseman727@gmail.com>
2025-03-09 15:57:59 +0900

    running commands via fork

--- main.c
+++ main.c
@@ -7,13 +7,10 @@
 #define MAX_CMD 256
 #define MAX_ARGCOUNT 256
 
-
-
-//По идее эта шутка должна вернуть массив с командой и с аргументами
-//Мб сделать структуру тип cmd??
 struct CMD 
 {
-    char* command;
+    //Мб command вообще не нужен...
+    //char* command;
     char* args[MAX_ARGCOUNT];
     //Как будто и без неё всё работает, но вдруг компилятор не будет делать null для ячеек массива, которые не использовались
     size_t argslen;
@@ -22,16 +19,17 @@ struct CMD
 struct CMD* parse_command(char* cmd_input) {
     struct CMD* result = malloc(sizeof(struct CMD));
     char* cmd_tok = strtok(cmd_input, " ");
-    size_t index = 0;
+    size_t index = 1;
 
-    result->command = cmd_tok;
+    result->args[0] = cmd_tok;
+    //result->command = cmd_tok;
     while (cmd_tok != NULL) {
-        
         cmd_tok = strtok(NULL, " ");
         result->args[index] = cmd_tok;
         ++index;
     }
     //Уменьшаем на единицу, чтобы не трогать элемент, идущий за последним аргументом
+    result->args[index] = NULL;
     result->argslen = --index;
     return result;
 
@@ -50,13 +48,24 @@ int main()
         scanf(" %256[^\n\r]", cmd_input);
 
         struct CMD* cmd = parse_command(cmd_input);
-        
-        printf("%s\n", cmd->command);
-        for (size_t index = 0; index < cmd->argslen; ++index) {
-            printf("%s ", cmd->args[index]);
 
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            fprintf(stderr, "Fork error\n");
+            exit(1);
+        } else if (pid == 0) {
+            char* path_env = getenv("PATH");
+            char* envp[] = {path_env, NULL};
+
+            int res = execvpe(cmd->args[0], cmd->args, envp);
+            printf("%d\n", res);
+            exit(0);
+        } else {
+            int pid_wait = wait(NULL);
         }
-        printf("\n");
+
+
         free(cmd);
     }
 

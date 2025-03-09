360774c5c46833ea5aaf06d6d2f740c9f34f898e
K <45696532+Pepseman727@users.noreply.github.com>
2025-03-09 22:14:46 +0900

    Update main.c

--- main.c
+++ main.c
@@ -76,7 +76,7 @@ int main()
     char cmd_input[MAX_CMD] = "";
 
     loop {
-        printf("%s ", prompt);
+        puts(prompt);
         scanf(" %256[^\n\r]", cmd_input);
         
         struct CMD* cmd = parse_command(cmd_input);
@@ -90,7 +90,7 @@ int main()
             perror("error run command");
         }
 
-        printf("\n");
+        puts("\n");
         free(cmd);
     }
 

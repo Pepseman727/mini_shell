262460428f754a8d30a72ac8658ca5b882d27a7d
Pepseman727 <pepseman727@gmail.com>
2025-03-09 15:15:16 +0900

    Вс 09 мар 2025 15:15:16 +09

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
-build: exe
+release: exe
+
+debug: exe_dbg
+
 
 exe: main.c
-	gcc main.c -o build/test_app
+	gcc main.c -O3 -o build/test_app
+exe_dbg: main.c
+	gcc main.c -o build/test_app_db -Wall
 
 clean:
 	rm -f build/* 2> /dev/null 
--- main.c
+++ main.c
@@ -3,27 +3,49 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define MAX_CMD 256
+#define MAX_ARGCOUNT 256
+
+
+
 //По идее эта шутка должна вернуть массив с командой и с аргументами
 //Мб сделать структуру тип cmd??
-void make_cmd() 
+/*void parse_command(char* cmd_input)  
 {
+    char* cmd_tok = NULL;
+    char* rest = cmd_input;
+    
+    while((cmd_tok = strtok_r(NULL, " ", &rest))) {
+        printf("%s\n", cmd_tok);
+    }
+
+}
+*/
+
+void parse_command(char* cmd_input) {
+    char* cmd_tok = strtok(cmd_input, " ");
+    while (cmd_tok != NULL) {
+        printf("\t%s\n", cmd_tok);
+        cmd_tok = strtok(NULL, " ");
+    }
+    
 
 }
 
 
 int main()
 {
-
+    
     //Пока не буду делать разницу между пользователями
     const char* prompt = "Enter ur command ->";
-    char cmd_input[256] = "";
-    char* cmd_tokenized = NULL;
-
+    char cmd_input[MAX_CMD] = "";
+    
     //Пока пиши как получается, потом отрефакторишь
     for(;;) {
-
         printf("%s ", prompt);
-        scanf("%256s", cmd_input);
+        scanf(" %256[^\n\r]", cmd_input);
+
+        parse_command(cmd_input);
         
         //Проблема в том, что на одной итерации, он считывает только одно слово, после этого он переходит на другую.
         //В итоге забывается предыдущая опция => Надо как-то считать всю строку так, чтоб она не перетиралась и осталось всё в одной итерации
